aa9aed4aaef09768b12bcea2415e6de33e2d05b0
oscargon12 <oscargon12@gmail.com>
2022-08-16 17:07:20 -0500

    Ejercicio peso

--- java_basics/src/_13Exercise.java
+++ java_basics/src/_13Exercise.java
@@ -4,17 +4,39 @@ import java.util.Scanner;
 
 public class _13Exercise {
 
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     // double unitKilogram = 0;
-    public double calcWeight() {
-        double unitKilogram = Double.parseDouble(sc.nextLine());
+    public static void calcWeight(double unitKilogram, int option) {
+        // double unitKilogram = Double.parseDouble(sc.nextLine());
 
-        return 12;
+        switch (option) {
+            case 1:
+                System.out.println(unitKilogram + " kilos " + " son " + (unitKilogram / 1000) + " toneladas");
+                break;
+            case 2:
+                System.out.println(unitKilogram + " kilos " + " son " + (unitKilogram / 0.1) + " hectogramos");
+                break;
+            case 3:
+                System.out.println(unitKilogram + " kilos " + " son " + (unitKilogram / 0.01) + " decagramos");
+                break;
+            case 4:
+                System.out.println(unitKilogram + " kilos " + " son " + (unitKilogram / 1000) + " gramos");
+                break;
+            default:
+                System.out.println("No ingresaste una opción valida");
+        }
+
     }
 
     public static void main(String[] args) {
         System.out.println("Selecciona la unidad a la que quieres convertir");
+        int option = Integer.parseInt(sc.nextLine());
+
         System.out.println(" 1. Tonelada \n 2. Hectogramo \n 3. Decagramo \n 4. Gramo");
+        System.out.println("Ingresa el peso en kilogramos");
+        double unitKilogram = Double.parseDouble(sc.nextLine());
+
+        calcWeight(unitKilogram, option);
     }
 }
